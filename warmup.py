668ae380e247275347e3b480552ed5a4ff7585cd
Jason <bjason@gmail.com>
2014-03-12 16:26:50 -0700

    Start of simple script

---
+++ warmup.py
@@ -0,0 +1,18 @@
+oldLines = []
+savedProdLines = []
+
+
+def readFile():
+    lines = open( "index.html", "r" )
+    array = []
+    for line in lines:
+        oldLines.append( line )
+
+
+
+
+import pprint
+
+pprint oldLines
+readFile()
+pprint oldLines
