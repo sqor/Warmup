6bec6b222025076868e5a033462bdde2c827d7a3
Jason <jason.bautista@sqor.com>
2014-03-13 11:54:16 -0700

    Basic script to compress .js files works.

--- js_minimizer.py
+++ js_minimizer.py
@@ -8,25 +8,49 @@ SRC_DELIMETER = "src="
 
 SCRIPT_TAG_DELIMETER = "<script"
 
-SCRIPT_BLOCK= False
-
 def get_script_src(line):
+    src = None
     if line.find(SCRIPT_TAG_DELIMETER) >=0:
-        pass
-    return None
+        soup = bs4.BeautifulSoup(line)
+        if soup.script != None:
+            srcRaw = soup.script.get('src')
+            # We want to ignore remote files
+            if srcRaw:
+                if srcRaw.find("http") == -1:
+                    src = srcRaw
 
-def readFile():
-    global SCRIPT_BLOCK
+    return src
 
+
+def write_file():
+    src = "google.com"
+    return "<script src='%s'></script>\n" % (src)
+
+def read_file():
+    global scriptSources
+    SCRIPT_BLOCK = False
     lines = open( "index.html", "r" )
     array = []
     for line in lines:
-        pass
+        src = get_script_src(line)
+        if src:
+            SCRIPT_BLOCK = True
+            scriptSources.append(src)
+        else:
+            SCRIPT_BLOCK = False
+        if len(scriptSources)> 0  and not SCRIPT_BLOCK:
+            # Write out our compiled css
+            new_script_tag = write_file()
+            scriptSources = []
+            newLines.append(new_script_tag)
+        # Write the line out
+        if not SCRIPT_BLOCK:
+            newLines.append(line)
 
 # Read file and parse
-readFile()
+read_file()
 # Write File
-f = open('index2.html','w')
+f = open('index3.html','w')
 for line in newLines:
     f.write(line)
 f.close()
