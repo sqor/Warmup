81094da623591167785dcc36b7b19826535f7a08
Jason <bjason@gmail.com>
2014-03-12 17:12:30 -0700

    prototype of script

--- warmup.py
+++ warmup.py
@@ -1,5 +1,6 @@
-oldLines = []
+newLines= []
 savedProdLines = []
+localLines = []
 
 START_LOCAL_DELIMETER = "<!--:START-LOCAL:-->"
 END_LOCAL_DELIMETER = "<!--:END-LOCAL:-->"
@@ -16,7 +17,8 @@ def readFile():
     array = []
     for line in lines:
 
-
+        # First we make sure to remove the old prod comments
+        # so the lines in between can show
         if line.find(START_PROD_DELIMETER) >=0:
             IS_PROD = True
             next(lines)
@@ -25,23 +27,19 @@ def readFile():
             IS_PROD = False
             next(lines)
 
-
-
-        if IS_PROD:
-            if not IS_LOCAL:
-                oldLines.append( line )
-                    #savedProdLines.append( line )
-
+        # Next we make sure to now ignore all the local script tags
         if line.find(START_LOCAL_DELIMETER) >=0:
             IS_LOCAL = True
+            next(lines)
 
         if line.find(END_LOCAL_DELIMETER) >=0:
             IS_LOCAL = False
+            next(lines)
 
-
-        if line.find(END_PROD_DELIMETER) >=0:
-            IS_PROD = False
-
+        if not IS_LOCAL:
+            newLines.append( line )
+        else:
+            localLines.append( line )
 
 
 
